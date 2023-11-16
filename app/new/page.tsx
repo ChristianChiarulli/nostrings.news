760f07ee78f8b2500f3627693a78bf967649ce61
Christian Chiarulli <chris.machine@pm.me>
2023-11-16 03:19:06 -0500

    fix: remove mounted nonsense

--- app/new/page.tsx
+++ app/new/page.tsx
@@ -1,18 +1,9 @@
-"use client";
-
-import { useEffect, useState } from "react";
 import NewPosts from "@/components/posts/NewPosts";
 
 export default function NoStrings() {
-  const [mounted, setMounted] = useState(false);
-
-  useEffect(() => {
-    setMounted(true);
-  }, []);
-
   return (
     <main className="flex min-h-screen flex-col items-start py-2">
-      {mounted && <NewPosts />}
+      <NewPosts />
     </main>
   );
 }
