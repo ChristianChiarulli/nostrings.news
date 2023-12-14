6d2e68a98eb52ee1bcf5b3f2197450d85517a327
Christian Chiarulli <chris.machine@pm.me>
2023-12-14 06:19:02 -0500

    feat: link for title as well

--- src/components/posts/PostTitle.tsx
+++ src/components/posts/PostTitle.tsx
@@ -1,15 +1,17 @@
 import { tag } from "~/lib/nostr";
 import Link from "next/link";
 import { type Event } from "nostr-tools";
+import useExternalLink from "~/hooks/useExternalLink";
 
 type Props = {
   postEvent: Event;
 };
 
 export const PostTitle = ({ postEvent }: Props) => {
+  const { href } = useExternalLink(postEvent);
   return (
     <Link
-      href={tag("u", postEvent) ?? "#"}
+      href={href}
       target="_blank"
       rel="nofollow noopener noreferrer"
     >
